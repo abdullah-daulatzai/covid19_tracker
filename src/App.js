ad4aa3ba1547759093151c0fb2dbe428f93af31e
Abdullah Daulatzai <abdullahdaulatzai1@gmail.com>
2025-01-07 01:39:01 +1300

    fix the layout for the logo

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import React from "react";
+import React from 'react'
 
-import { Cards, Country, Chart } from "./components";
-import { fetchData } from "./api/";
-import styles from "./App.module.css";
-import coronaImage from "./images/image.png";
+import { Cards, Country, Chart } from './components'
+import { fetchData } from './api/'
+import styles from './App.module.css'
+import coronaImage from './images/image.png'
 
 class App extends React.Component {
   state = {
     data: {},
-    country: "",
-  };
+    country: '',
+  }
 
   async componentDidMount() {
-    const data = await fetchData();
+    const data = await fetchData()
 
-    this.setState({ data });
+    this.setState({ data })
   }
 
   handleCountryChange = async (country) => {
-    const data = await fetchData(country);
+    const data = await fetchData(country)
 
-    this.setState({ data, country: country });
-  };
+    this.setState({ data, country: country })
+  }
 
   render() {
-    const { data, country } = this.state;
+    const { data, country } = this.state
 
     return (
       <div className={styles.container}>
+        <br></br>
+        <br></br>
+        <br></br>
         <img className={styles.image} src={coronaImage} alt="Covid19" />
         <Cards data={data} />
         <Country handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
